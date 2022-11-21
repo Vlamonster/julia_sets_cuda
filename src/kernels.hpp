8dd961de8e6e94f1d2886b244cdf7c7d18676695
Vlamonster <78276563+Vlamonster@users.noreply.github.com>
2022-11-21 18:08:12 +0100

    Initial commit

---
+++ src/kernels.hpp
@@ -0,0 +1,23 @@
+__global__ void
+iterateCoordinates( float *d_coords,
+                    int *d_steps,
+                    const long width,
+                    const long height,
+                    const float c_x,
+                    const float c_y,
+                    const long iteration);
+
+__global__ void
+initializeCoords( float *d_coords,
+                  const long width,
+                  const long height,
+                  const float top,
+                  const float bottom,
+                  const float left,
+                  const float right);
+
+__global__ void
+setColors(const int *d_steps,
+          char *d_colors,
+          const long width,
+          const long height);
